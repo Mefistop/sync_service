84393df3ef90ef40634e1f4df21fb032ee0e68e8
Petr Baikov <pbaqkov@gmail.com>
2025-04-24 21:28:52 +0300

    Fix: cloadapi

--- cloud_sync.py
+++ cloud_sync.py
@@ -6,6 +6,7 @@ url_files = 'https://cloud-api.yandex.net/v1/disk/resources/files'
 
 class CloudStorageApi:
 
+
     def __init__(self, token: str, remote_folder_name: str = "disk:"):
         self.headers = {"Accept": "application/json", "Authorization": f"OAuth {token}"}
         self.base_url = 'https://cloud-api.yandex.net/v1/disk/resources/'
@@ -21,10 +22,7 @@ class CloudStorageApi:
             headers=self.headers,
             **kwargs,
         )
-        if response.status_code >= 400:
-            raise Exception(f'Api request failed: {response.status_code} - {response.text}')
-        if response.text:
-            return response.json()
+        return response
 
 
     def get_info(self):
@@ -33,11 +31,17 @@ class CloudStorageApi:
             "fields": "items.name,items.path,items.size, items.modified",
         }
         response = self._make_request(method='get', endpoint='files', params=params)
-        data = []
-        for file in response['items']:
+
+        if response.status_code >= 400:
+            print('Не удалось установить подключиться Яндекс Диску. Ошибка соединения.')
+            return
+
+        response_data = response.json()
+        file_info_data = []
+        for file in response_data['items']:
             if file["path"].startswith(f"disk:{self.remote_folder_name}"):
-                data.append(file)
-        return data
+                file_info_data.append(file)
+        return file_info_data
 
 
     def load(self, local_file_path: str, remote_file_name: str, overwrite: bool = False):
@@ -45,18 +49,24 @@ class CloudStorageApi:
             "path": f"{self.remote_folder_name}/{remote_file_name}",
             'overwrite': str(overwrite).lower()
         }
-        upload_info = self._make_request(method='get', endpoint='upload', params=params)
+        response_for_upload = self._make_request(method='get', endpoint='upload', params=params)
+        if response_for_upload.status_code >= 400:
+            print(f"Файл '{local_file_path.split('/')[-1]}' не {['записан', "перезаписан"][overwrite]}. Ошибка: {response_for_upload.json()['message']}")
+            return
+        upload_info = response_for_upload.json()
 
         with open(local_file_path, 'rb') as file:
             response = self.session.request(
                 method='put',
                 url=upload_info['href'],
                 data=file,
-                headers=self.headers
-
+                headers=self.headers,
             )
+
         if response.status_code not in (201, 202):
-            raise Exception(f'File upload failed: {response.status_code} - {response.text}')
+            print(f"Файл '{local_file_path.split('/')[-1]}' не {['записан', "перезаписан"][overwrite]}. Ошибка: {response.json()['message']}")
+            return
+        print(f"Файл '{local_file_path.split('/')[-1]}' успешно {['записан', "перезаписан"][overwrite]}.")
         return True
 
 
@@ -67,7 +77,11 @@ class CloudStorageApi:
     def delete(self, remote_file_name: str):
         params = {'path': f"{self.remote_folder_name}/{remote_file_name}"}
         response = self._make_request(method='delete', params=params)
-        print(response)
+        if response.status_code >= 400:
+            message = response.json()['message']
+            print(f"Файл '{remote_file_name}' не удален. Ошибка: {message}")
+            return
+        print(f"Файл '{remote_file_name}' успешно удален.")
 
 
 
@@ -85,15 +99,13 @@ if __name__ == "__main__":
             local_file_path="/home/user/Downloads/Техническое задание «Сервис синхронизации файлов».docx",
             remote_file_name="Техническое задание «Сервис синхронизации файлов».docx",
         )
-    print("Файл успешно загружен!")
-
+    # #
     # Проверка reload
     print("Проверка reload")
     cloud.reload(
             local_file_path="/home/user/Downloads/Техническое задание «Сервис синхронизации файлов».docx",
             remote_file_name="Техническое задание «Сервис синхронизации файлов».docx",
         )
-    print("Файл успешно перезагружен!")
     print('Проверка delete')
     cloud.delete(remote_file_name='Техническое задание «Сервис синхронизации файлов».docx')
 
